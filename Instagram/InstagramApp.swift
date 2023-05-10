dab162d2cf7a39155afee2b11fcdb9954337955a
Nitesh Kumar <niteshdangi28@gmail.com>
2023-05-10 20:28:08 +0530

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Nitesh Kumar3 on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
